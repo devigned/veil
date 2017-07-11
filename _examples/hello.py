3f1ca3209a1c6e9388a49796ebe7e5f54dff9053
David Justice <david@devigned.com>
2017-07-11 16:58:35 -0700

    working cffi example to expand upon

--- _examples/hello.py
+++ _examples/hello.py
@@ -1,28 +1,35 @@
 import os
+import subprocess
+import re
 from cffi import FFI
 
 here = os.path.dirname(os.path.abspath(__file__))
 header_path = os.path.join(here, "_hello.h")
 
-ffibuilder = FFI()
-ffibuilder.cdef("""
-int GetMagicNumber();
-""")
+ffi_builder = FFI()
+expanded_header = subprocess.Popen([
+    'cc', '-E', header_path],
+    stdout=subprocess.PIPE).communicate()[0].decode()
+
+patterns = '.+?_check_for_64_bit_pointer_matching_GoInt|.+?_Complex'
+filtered_header = [line for line in expanded_header.split("\n")
+                   if re.match(patterns, line) is None]
+ffi_builder.cdef("\n".join(filtered_header))
+
 
 # python <--> cffi helper.
-class cffi_helper(object):
+class FFIHelper(object):
     here = os.path.dirname(os.path.abspath(__file__))
-    lib = ffibuilder.dlopen(os.path.join(here, "_hello.so"))
+    lib = ffi_builder.dlopen(os.path.join(here, "_hello.so"))
 
     @staticmethod
     def get_magic_number():
-        return cffi_helper.lib.GetMagicNumber()
+        return FFIHelper.lib.GetMagicNumber()
+
 
 def main():
-  print(cffi_helper.get_magic_number())
-  # j = 0
-  # for i in range(10000000):
-  #   j += 1
+    print(FFIHelper.get_magic_number())
+
 
 if __name__ == "__main__":
     main()
--- _examples/hello_build.py
+++ _examples/hello_build.py
@@ -4,7 +4,8 @@ from cffi import FFI
 ffibuilder = FFI()
 here = os.path.dirname(os.path.abspath(__file__))
 header_path = os.path.join(here, "_hello.h")
-ffibuilder.set_source("_hello_example", open(header_path, "r").read(), libraries=[os.path.join(here, "hello")])
+ffibuilder.set_source("_hello_example", open(header_path, "r").read(),
+                      libraries=[os.path.join(here, "hello")])
 ffibuilder.cdef("""
     int GetMagicNumber();
 """)
