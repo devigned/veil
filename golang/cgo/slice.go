24c8cadad75df514896e1888f410ccf99340b349
David Justice <david@devigned.com>
2017-07-18 08:52:34 -0700

    add slice append

--- golang/cgo/slice.go
+++ golang/cgo/slice.go
@@ -34,6 +34,7 @@ func (s SliceWrapper) ToCgoAst() []ast.Decl {
 	decls = append(decls, s.StringAst()...)
 	decls = append(decls, s.ItemAst()...)
 	decls = append(decls, s.ItemSetAst()...)
+	decls = append(decls, s.ItemAppendAst()...)
 	return decls
 }
 
@@ -244,6 +245,63 @@ func (s SliceWrapper) ItemSetAst() []ast.Decl {
 					},
 					Rhs: []ast.Expr{
 						itemIdent,
+					},
+					Tok: token.ASSIGN,
+				},
+			},
+		},
+	}
+
+	return []ast.Decl{funcDecl}
+}
+
+func (s SliceWrapper) ItemAppendAst() []ast.Decl {
+	functionName := s.CGoName() + "_item_append"
+	selfIdent := NewIdent("self")
+	goTypeIdent := NewIdent(s.GoName())
+	elementTypeIdent := NewIdent(s.elem.String())
+	itemsIdent := NewIdent("items")
+	itemIdent := NewIdent("item")
+
+	castExpression := CastUnsafePtr(DeRef(goTypeIdent), selfIdent)
+
+	funcDecl := &ast.FuncDecl{
+		Doc: &ast.CommentGroup{
+			List: ExportComments(functionName),
+		},
+		Name: NewIdent(functionName),
+		Type: &ast.FuncType{
+			Params: InstanceMethodParams(goTypeIdent,
+				[]*ast.Field{
+					{
+						Names: []*ast.Ident{itemIdent},
+						Type:  elementTypeIdent,
+					},
+				}...),
+		},
+		Body: &ast.BlockStmt{
+			List: []ast.Stmt{
+				&ast.AssignStmt{
+					Lhs: []ast.Expr{
+						itemsIdent,
+					},
+					Rhs: []ast.Expr{
+						castExpression,
+					},
+					Tok: token.DEFINE,
+				},
+				&ast.AssignStmt{
+					Lhs: []ast.Expr{
+						DeRef(itemsIdent),
+					},
+					Rhs: []ast.Expr{
+						&ast.CallExpr{
+							Fun: NewIdent("append"),
+							Args: []ast.Expr{
+								DeRef(itemsIdent),
+								itemIdent,
+							},
+						},
 					},
 					Tok: token.ASSIGN,
 				},
