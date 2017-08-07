232bfe30068ed2b3fd556eef1e0186b700f140e8
David Justice <david@devigned.com>
2017-08-07 12:52:11 -0700

    clean import test

--- cgo/import_test.go
+++ cgo/import_test.go
@@ -9,15 +9,13 @@ import (
 func TestSingleImport(t *testing.T) {
 	subject := Imports("C")
 	assert.NotNil(t, subject)
-	genDecl := subject
-	assert.Equal(t, 1, len(genDecl.Specs))
-	importSpec := genDecl.Specs[0].(*ast.ImportSpec)
+	assert.Equal(t, 1, len(subject.Specs))
+	importSpec := subject.Specs[0].(*ast.ImportSpec)
 	assert.Equal(t, "\"C\"", importSpec.Path.Value)
 }
 
 func TestMultipleImports(t *testing.T) {
 	subject := Imports("foo", "bar", "baz")
 	assert.NotNil(t, subject)
-	genDecl := subject
-	assert.Equal(t, 3, len(genDecl.Specs))
+	assert.Equal(t, 3, len(subject.Specs))
 }
